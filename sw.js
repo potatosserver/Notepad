c40531fe76affaa90478c105316e377ad2cd9d20
potatosserver <potatosserver@gmail.com>
2025-10-16 23:00:40 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'notepad-pwa-cache-v1.1';
+const urlsToCache = [
+  '/',
+  './index.html',
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
+];
+
+// 安裝 Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 攔截網路請求並從快取提供資源
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 如果快取中有對應的資源，就直接回傳
+        if (response) {
+          return response;
+        }
+        // 否則，從網路請求
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// 更新 Service Worker 時，刪除舊的快取
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
